0a97911263a5b60e1e78252d7060a7a197b40df7
KingBrink <kyle.brink1602@gmail.com>
2024-08-19 15:02:02 +0200

    edits1

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,19 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <NavBar />
+  <main>
+    <router-view />
+  </main>
+  <Footer/>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import NavBar from './components/NavBar.vue'
+import Footer from '@/components/Footer.vue'
+export default {
+  components: {
+    NavBar,
+    Footer
+  }
 }
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+</script>
+<style src="@/assets/css/style.css">
 </style>
