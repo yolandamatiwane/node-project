d43ba3d1a91521f22a83c937d372cc79555e8b6b
KingBrink <kyle.brink1602@gmail.com>
2024-08-19 13:20:31 +0200

    componentsdone

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,1 +1,17 @@
-<!-- footer -->
+<template>
+    <div class="container-fluid">
+        <div class="row">
+            <p class="lead">
+                JT &copy; <span id="currYear"></span>
+            </p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "FooterComp",
+}
+</script>
+
+<style scoped></style>
--- src/components/Spinner.vue
+++ src/components/Spinner.vue
@@ -1,1 +1,11 @@
-<!-- spinner -->
+<template>
+    <div class="d-flex justify-content-center">
+        <div class="spinner-border" role="status">
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'SpinnerComp'
+}
+</script>
