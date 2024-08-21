3a313c205b785541f5ad29194adf0fe974a37667
KingBrink <kyle.brink1602@gmail.com>
2024-08-21 12:43:41 +0200

    admintable

---
+++ src/components/AddUser.vue
@@ -0,0 +1,156 @@
+<template>
+    <button
+      type="button"
+      class="btn"
+      data-bs-toggle="modal"
+      data-bs-target="#exampleModal"
+    >
+      Add User
+    </button>
+    <div
+      class="modal fade"
+      id="exampleModal"
+      tabindex="-1"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div class="modal-body">
+            <form>
+              <div class="mb-3">
+                <input
+                  type="text"
+                  class="form-control w-50 mx-auto"
+                  placeholder="user ID"
+                  v-model="payload.userID"
+                />
+              </div>
+              <div class="mb-3">
+                <input
+                  type="text"
+                  class="form-control w-50 mx-auto"
+                  placeholder="first name"
+                  v-model="payload.firstName"
+                />
+              </div>
+              <div class="mb-3">
+                <input
+                  type="text"
+                  class="form-control w-50 mx-auto"
+                  placeholder="last name"
+                  v-model="payload.lastName"
+                />
+              </div>
+              <div class="mb-3">
+                <input
+                  type="text"
+                  class="form-control w-50 mx-auto"
+                  placeholder="user age"
+                  v-model="payload.userAge"
+                />
+              </div>
+              <div class="mb-3">
+                <input
+                  type="text"
+                  class="form-control w-50 mx-auto"
+                  placeholder="gender"
+                  v-model="payload.Gender"
+                />
+              </div>
+              <div class="mb-3">
+                <input
+                  type="text"
+                  class="form-control w-50 mx-auto"
+                  placeholder="user role"
+                  v-model="payload.userRole"
+                />
+              </div>
+              <div class="mb-3">
+                <input
+                  type="text"
+                  class="form-control w-50 mx-auto"
+                  placeholder="email address"
+                  v-model="payload.emailAdd"
+                />
+              </div>
+              <div class="mb-3">
+                <input
+                  type="text"
+                  class="form-control w-50 mx-auto"
+                  placeholder="user password"
+                  v-model="payload.userPass"
+                />
+              </div>
+              <div class="mb-3">
+                <input
+                  type="text"
+                  class="form-control w-50 mx-auto"
+                  placeholder="user profile"
+                  v-model="payload.userProfile"
+                />
+              </div>
+              <div class="modal-footer">
+                <button
+                  type="button"
+                  class="btn btn-secondary"
+                  data-bs-dismiss="modal"
+                >
+                  Close
+                </button>
+                <button
+                  type="submit"
+                  @click.prevent="addingUser"
+                  class="btn btn-success"
+                  data-bs-dismiss="modal"
+                >
+                  Add User
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "addProduct",
+    data() {
+      return {
+        payload: {
+          userID: null,
+          firstName: "",
+          lastName: "",
+          userAge: null,
+          Gender: "",
+          userRole: "",
+          emailAdd: "",
+          userPass: "",
+          userProfile: "",
+        },
+      };
+    },
+    methods: {
+      addingUser() {
+        try {
+          this.$store.dispatch("register", this.payload);
+        } catch (error) {
+          console.error(error);
+        }
+      },
+    },
+  };
+  </script>
+  
+  <style scoped></style>
--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -1,14 +1,149 @@
-<!-- admin -->
- <template>
-   hehe
- </template>
- 
- <script>
- export default {
- 
- }
- </script>
- 
- <style>
- 
- </style>
+<template>
+  <div>
+<!-- USERS TABLE -->
+<br><h2>Users Table</h2><br>
+<addUser/>
+  <div class="container table-responsive">
+    <table class="table">
+    <thead class="table-dark">
+      <tr>
+        <th>User ID</th>
+        <th>First Name</th>
+        <th>Last Name</th>
+        <th>User Age</th>
+        <th>Gender</th>
+        <th>User Role</th>
+        <th>Email Address</th>
+        <th>User Password</th>
+        <th>User Profile</th>
+        <th>
+          Action
+        </th>
+      </tr>
+    </thead>
+  <tbody v-if="users">
+<tr v-for="user in users" :key="user.userID">
+
+    <td>{{ user.userID }}</td>
+    <td>{{ user.firstName }}</td>
+    <td>{{ user.lastName }}</td>
+    <td>{{ user.userAge }}</td>
+    <td>{{ user.Gender }}</td>
+    <td>{{ user.userRole }}</td>
+    <td>{{ user.emailAdd }}</td>
+    <td>{{ user.Pass }}</td>
+    <td><img :src="user.userProfile" alt="" style="width: 35px;" loading="lazy"></td>
+    <td class="d-flex justify-content-between">
+      <updateUser :user="user" />
+      <button class="btn btn-success deleteButton" @click="event => deleteUser(user.userID)">Delete</button>
+    </td>
+  </tr>
+</tbody>
+</table>
+</div>
+<!-- PRODUCTS TABLE -->
+<br><h2>Products Table</h2><br>
+<addProduct/>
+  <div class="container table-responsive">
+    <table class="table">
+    <thead class="table-dark">
+      <tr>
+        <th>Product ID</th>
+        <th>Product Name</th>
+        <th>Quantity</th>
+        <th>Amount</th>
+        <th>Category</th>
+        <th>Product URL</th>
+        <th>Product Description</th>
+        <th>
+            Action
+        </th>
+    </tr>
+</thead>
+  <tbody v-if="products">
+    <tr v-for="product in products" :key="product.prodID">
+        <td>{{ product.prodID }}</td>
+        <td>{{ product.prodName }}</td>
+        <td>{{ product.quantity }}</td>
+        <td>R {{ product.amount }}</td>
+        <td>{{ product.category }}</td>
+        <td><img :src="product.prodUrl" loading="lazy" class="img-fluid" style="width: 60px;" :alt="product.prodName"></td>
+        <td>{{ product.prodDesc }}</td>
+        <td class="d-flex justify-content-between">
+            <updateProduct :product="product" />
+            <button class="btn btn-success deleteButton" @click="event => deleteProduct(product.prodID)">Delete</button>
+        </td>
+    </tr>
+  </tbody>
+</table>
+</div>
+</div>
+</template>
+
+<script>
+import updateUser from '@/components/UpdateUserModal.vue';
+import updateProduct from '@/components/UpdateProduct.vue';
+import addProduct from '@/components/AddProduct.vue';
+import addUser from '@/components/AddUser.vue';
+
+  export default {
+    components: {
+      updateUser,
+      updateProduct,
+      addProduct,
+      addUser,
+    },
+    computed: {
+      products(){
+        return this.$store.state.products
+    },
+      users(){
+        return this.$store.state.users
+      }
+    },
+    mounted() {
+    this.$store.dispatch('fetchProducts'),
+    this.$store.dispatch('fetchUsers')
+}, 
+methods: {
+  deleteUser(userID) {
+    this.$store.dispatch('deleteUser', {id: userID});
+  },
+  deleteProduct(prodID) {
+    this.$store.dispatch('deleteProduct', {id: prodID});
+  },
+  updateUser(user) {
+    let editUser = {userID: user.userID, firstName: user.firstName, lastName: user.lastName, userAge: user.userAge, Gender: user.Gender, userRole: user.userRole, emailAdd: user.emailAdd, userPass: user.userPass, userProfile: user.userProfile}
+  this.$store.dispatch('updateUser', {id: user.userID, data: editUser})
+  },
+  updateProduct(product) {
+    let editProduct = {productID: product.prodID, productName: product.prodName, quantity: product.quantity, amount: product.amount, Category: product.Category, prodURL: product.prodURL, prodDesc: product.prodDesc}
+  this.$store.dispatch('updateProduct', {id: product.prodID, data: editProduct})
+}}
+}
+</script>
+
+<style scoped>
+td {
+padding: 10px;
+}
+@media(max-width:351px){
+td {
+  padding: 20px;
+}
+}
+/* .btn {
+border-radius: 30px;
+color: #fff;
+background-color: #4E96D3;
+font-size: 12px;
+border: none;
+cursor: pointer;
+transition: .4s;
+}
+.btn:hover{
+background-color: #1d3557;
+color: #fff;
+transition: 1s;
+} */
+</style>
